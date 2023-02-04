42de5580bf98a829600ee2c31cc133d0c72b043e
Balint Hegyi <bhegyi@gradle.com>
2023-02-04 13:50:29 +0000

    Fix formatting, change templates

--- lib/recipier_web/live/home.ex
+++ lib/recipier_web/live/home.ex
@@ -2,13 +2,8 @@ defmodule RecipierWeb.Home do
   use RecipierWeb, :live_view
 
   def mount(_params, _session, socket) do
-    today = Date.utc_today()
-
-    days =
-      Date.range(
-        Date.beginning_of_week(today),
-        Date.end_of_week(today)
-      )
+    start_day = Date.beginning_of_week(Date.utc_today())
+    weekdays = get_weekdays(start_day)
 
     mealtimes = [
       %{
@@ -25,11 +20,28 @@ defmodule RecipierWeb.Home do
       }
     ]
 
-    {:ok, assign(socket, days: days, mealtimes: mealtimes)}
+    {:ok, assign(socket, start_day: start_day, weekdays: weekdays, mealtimes: mealtimes)}
   end
 
-  def handle_event("toggle", %{days: days}, socket) do
-    {:noreply, assign(socket, :days, days)}
+  def handle_event("page_up", params, socket) do
+    %{assigns: %{start_day: start_day}} = socket
+    new_start_day = Date.add(start_day, 7)
+
+    {:noreply, assign(socket, start_day: new_start_day, weekdays: get_weekdays(new_start_day))}
+  end
+
+  def handle_event("page_down", params, socket) do
+    %{assigns: %{start_day: start_day}} = socket
+    new_start_day = Date.add(start_day, -7)
+
+    {:noreply, assign(socket, start_day: new_start_day, weekdays: get_weekdays(new_start_day))}
+  end
+
+  defp get_weekdays(start_day) do
+    Date.range(
+      Date.beginning_of_week(start_day),
+      Date.end_of_week(start_day)
+    )
   end
 
   defp format_date_segments(number) do
---
+++ priv/repo/migrations/20230204134401_initial.exs
@@ -0,0 +1,9 @@
+defmodule Recipier.Repo.Migrations.Initial do
+  use Ecto.Migration
+
+  def change do
+    create table("mealtimes") do
+      add
+    end
+  end
+end
